2d45197e82c0f20e0e829eb0aa42aba2b533cf5c
slyang08 <e2223845@gmail.com>
2026-02-27 12:25:38 -0500

    test: connect api and web by shared docs

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,25 @@
+import cors from "cors";
 import express from "express";
 
+import { Product } from "@liushushu/types";
+
 const app = express();
+app.use(cors());
 const PORT = 8080;
+
+const products: Product[] = [
+    { id: 1, name: "Musang King", price: 120 },
+    { id: 2, name: "D24", price: 80 },
+  ];
 
 app.get("/", (req, res) => {
   res.send("Durian Tree API 🌳");
 });
 
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
